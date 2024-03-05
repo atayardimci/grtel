bbdf2cc01ae970729372a1f27c8bd8be42d4db69
Ata <atayardimci@roaminsight.com>
2024-03-05 01:27:51 +0000

    updates to classification

--- src/grtel/classification.py
+++ src/grtel/classification.py
@@ -13,14 +13,14 @@ class GRTEL:
     """Graph Regularized Tensor Ensemblef Learning"""
     @beartype
     def __init__(self,
-        base_clfs,
-        n_classes: int = 1,
+        base_clfs,  # TODO: typing
         probability: bool = False,
+        n_classes: int = 1,
         verbose: bool = False,
     ):
         self.probability = probability
-        self.verbose = verbose
         self.n_classes = n_classes
+        self.verbose = verbose
         self.models = [
             TelVI(
                 base_clf=base_clf,
@@ -31,41 +31,32 @@ class GRTEL:
         ]
 
     @beartype
-    def fit(self, X, y):
-        if self.n_classes == 1:
-            self.models[0].fit(X, y)
-        elif self.n_classes > 1:
-            for i in range(self.n_classes):
-                print(i, end=" - ")
-                self.models[i].fit(X, y[:, i])
+    def fit(self, X: pd.DataFrame, y: np.ndarray) -> None:
+        for i in range(self.n_classes):
+            print(i, end=" - ")
+            self.models[i].fit(X, y[:, i])
         print()
 
     @beartype
-    def score(self, X, y):
-        if self.n_classes:
-            return [self.models[0].score(X, y)]
-        elif self.n_classes > 1:
-            return [self.models[i].score(X, y[:, i]) for i in range(self.n_classes)]
+    def score(self, X: pd.DataFrame, y: np.ndarray) -> list[float]:
+        return [self.models[i].score(X, y[:, i]) for i in range(self.n_classes)]
 
     @beartype
-    def grid_search(self, X, y, search_params):
-        if self.n_classes == 1:
-            self.models[0].grid_search(X, y, search_params)
-        elif self.n_classes > 1:
-            for i in range(self.n_classes):
-                print(i, end=" - ")
-                self.models[i].grid_search(X, y[:, i], search_params)
+    def grid_search(self, X: pd.DataFrame, y: np.ndarray, search_params) -> None:
+        for i in range(self.n_classes):
+            print(i, end=" - ")
+            self.models[i].grid_search(X, y[:, i], search_params)
         print()
 
     @beartype
-    def predict(self, X):
+    def predict(self, X: pd.DataFrame) -> list[np.ndarray]:
         return [
             self.models[i].predict(X)
             for i in range(self.n_classes)
         ]
 
     @beartype
-    def confusion_matrices(self, X, y):
+    def confusion_matrices(self, X: pd.DataFrame, y: np.ndarray) -> list[np.ndarray]:
         predictions = self.predict(X)
         return [
             confusion_matrix(y[:, i], predictions[i])
